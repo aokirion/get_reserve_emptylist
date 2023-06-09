31873b682114f444d901c91fa20748347328b98e
aokirion <102980879+aokirion@users.noreply.github.com>
2023-06-09 17:32:55 +0900

    Update line_notify.py

--- line_notify.py
+++ line_notify.py
@@ -10,10 +10,8 @@ def send_line_notify(notification_message):
     for value in notification_message:
         data_list = data_list + "\n" + value
 
-    line_notify_token = '6E2PjPFVVX8W3eO8xYcCEX8HKALaxpbQiUSIx1oCLNm'
-    # 6E2PjPFVVX8W3eO8xYcCEX8HKALaxpbQiUSIx1oCLNm
+    line_notify_token = 'トークンを記載する'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': f' {data_list}'}
     requests.post(line_notify_api, headers = headers, data = data)
-    # requests.post(line_notify_api, data = data)
